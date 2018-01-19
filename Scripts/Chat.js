b0fce684f842c33971a24aca3ad65a591ad66b4c
rkolen <rkolen2007@gmail.com>
2018-01-19 12:13:12 +0100

    improved readability

--- Scripts/Chat.js
+++ Scripts/Chat.js
@@ -1,16 +1,13 @@
 
 var xhr = new XMLHttpRequest();
 var key = "raymond";
-// for the retrieve messages function
+// for the retrieve messages function change 0 to avoid seeing really old messages
 var highestId = 0;
-var correctids = [555, 558, 566, 714, 750, 758];
 
 $(function() {
 	var messageScreen = document.getElementById("chat-messages");
 	// attach username to submit and start the session
-
 	$("#textbox").keypress(function(event) {
-
 		if(event.which == 13) {	
 		//looks for enter key
 			if ( $("#enter").prop("checked") ) {
@@ -18,12 +15,10 @@ $(function() {
 				//console.log("enter pressed, checkbox is checked");
 				$("#send").click();
 				event.preventDefault();
-
 			}
-
 		}
-		
 	});
+
 	// function sends message to the server and displays in chat-messages
 	$("#send").click(function() {
 		//user name added to message
@@ -31,66 +26,62 @@ $(function() {
 		var allMessage = $("#chat-messages").html();
 		var newMessage = $("#textbox").val();
 		//console.log(userID);
-			username = "<span class = 'username' >" + userID + " says: </span>";
+		username = "<span class = 'username' >" + userID + " says: </span>";
 		//function to clear textbox after sending message
 		$("#textbox").val("");
-
 		//sending to server username+message
-			xhr.open("POST", "https://www.codegorilla.nl/read_write/api.php?action=write&mykey="+ key +"&value=" + username + newMessage, false);
-			xhr.send ();
-			//to see message id in the console
-			console.log(xhr.response);
+		xhr.open("POST", "https://www.codegorilla.nl/read_write/api.php?action=write&mykey="+ key +"&value=" + username + newMessage, false);
+		xhr.send ();
 		//show all messages
 		$("#chat-messages").html(allMessage);
 		//scrolls down on send automatically
-			scrollToBottom();
-	
+		scrollToBottom();
 	});
+
 	//scrolls to the bottom of the page
-function scrollToBottom(){
-	$("#chat-messages").scrollTop($("#chat-messages").prop("scrollHeight"));
-}
-//retrieves from the server
-function grabMessageById(id){
-	xhr.open("GET", "https://codegorilla.nl/read_write/api.php?action=read&mykey=" + key + "&id=" + id, false);
-	xhr.send();
-}
-	//function to refresh and show old messages
-function refreshChat() {
-	var messageScreen = document.getElementById("chat-messages");
-		for (i = 0; i < correctids.length; i++) {
-			if (correctids[i] > highestId) {
-				grabMessageById(correctids[i]); // id is not defined
-				var newMessage = xhr.response;
+	function scrollToBottom() {
+		$("#chat-messages").scrollTop($("#chat-messages").prop("scrollHeight"));
+	}
 
-				messageScreen.innerHTML += newMessage + "<br>";
+	//retrieves from the server
+	function grabMessageById(id) {
+		xhr.open("GET", "https://codegorilla.nl/read_write/api.php?action=read&mykey=" + key + "&id=" + id, false);
+		xhr.send();
+	}
 
-				scrollToBottom("chat-messages");
-				highestId = correctids[i];
-				console.log(highestId);
+	// gets the old messages from server
+	function getCorrectIds() {
+		xhr.open("GET" , "https://codegorilla.nl/read_write/api.php?action=list&mykey=" + key, false);
+		xhr.send();
+		correctids = xhr.response;
+		//splits array of substrings
+		correctids = correctids.split(",");
+		// gets the id`s used by the specific key
+		for (i = 0 ; i < correctids.length; i++) {
+			correctids[i] = parseInt(correctids[i]);
 		}
 	}
-}
-// gets the old messages from server
-function getCorrectIds() {
-	xhr.open("GET" , "https://codegorilla.nl/read_write/api.php?action=list&mykey=" + key, false);
-	xhr.send();
-	correctids = xhr.response;
-	//splits array of substrings
-	correctids = correctids.split(",");
-	console.log(correctids);
-	// gets the id`s used by the specific key
-	for (i = 0 ; i < correctids.length; i++) {
-		correctids[i] = parseInt(correctids[i]);
-	}
-	console.log(correctids[i]);
-	console.log(typeof correctids[i]);
-}
-//refreshes window
-window.setInterval(function(){
-	getCorrectIds();
-	refreshChat();
-}, 6000);
 
+	//function to refresh and show old messages
+	function refreshChat() {
+		var messageScreen = document.getElementById("chat-messages");
+			for (i = 0; i < correctids.length; i++) {
+				if (correctids[i] > highestId) {
+					grabMessageById(correctids[i]);
+					var newMessage = xhr.response;
+					//line break for messages
+					messageScreen.innerHTML += newMessage + "<br>";
+					// to see latest messages
+					scrollToBottom("chat-messages");
+					highestId = correctids[i];
+				}
+			}
+	}
+
+	//refreshes window
+	window.setInterval(function() {
+		getCorrectIds();
+		refreshChat();
+	}, 6000);
 		
 });
